8986eb34cfc46209b157e9ae6e0fdb7b0b12eb3f
Oto Hlincik <ohlincik@gmail.com>
2014-07-30 16:42:22 -0400

    Apply new base layout
     - New layout
     - Clean up devise views
     - Improve user profile view

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,6 +3,6 @@ module UsersHelper
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    image_tag(gravatar_url, alt: user.name, class: "img-thumbnail")
   end
 end
