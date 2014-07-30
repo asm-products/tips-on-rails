e307b4d921c0d8c4b8d8afd25ef25d42916ac8bc
Oto Hlincik <ohlincik@gmail.com>
2014-07-30 10:26:51 -0400

    Fix migrations related to Bookmarks table

--- db/migrate/20140728152552_add_id_to_tips.rb
+++
@@ -1,5 +0,0 @@
-class AddIdToTips < ActiveRecord::Migration
-  def change
-  	add_column :tips, :tip_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140728152552) do
+ActiveRecord::Schema.define(version: 20140728150358) do
 
   create_table "bookmarks", force: true do |t|
     t.integer  "tip_id"
@@ -20,18 +20,18 @@ ActiveRecord::Schema.define(version: 20140728152552) do
     t.datetime "updated_at"
   end
 
+  add_index "bookmarks", ["tip_id"], name: "index_bookmarks_on_tip_id", using: :btree
+  add_index "bookmarks", ["user_id"], name: "index_bookmarks_on_user_id", using: :btree
+
   create_table "tips", force: true do |t|
-    t.text     "title"
-    t.string   "description"
+    t.string   "title"
+    t.text     "description"
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "code"
     t.string   "references"
-    t.integer  "tip_id"
   end
-
-  add_index "tips", ["user_id", "created_at"], name: "index_tips_on_user_id_and_created_at", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "first_name"
