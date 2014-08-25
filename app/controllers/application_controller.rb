633d487fd0ebe2cb078a71d4508142d73bf38168
Mike Bukowski <michael.r.bukowski@gmail.com>
2014-08-25 14:20:45 -0400

    Add Admin functionality
     - Add authentication methods to application controller
     - Add Admin boolean to users table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_admin!
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -8,10 +9,21 @@ class ApplicationController < ActionController::Base
   def test_exception_notifier
     raise 'This is just a test to make sure Exception Notifier is working properly.'
   end
+end
+
 
 private
 
   def configure_permitted_parameters
   	devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:last_name, :first_name, :email, :password, :password_confirmation) }
 	end
+
+  def authenticate_admin!
+    if current_user.try(:admin?)
+    raise "error" unless current_user and current_user.admin?
+  end
+
+  # def authenticate_admin!
+  #   raise "error" unless current_user and current_user.admin?
+  # end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140805135256) do
+ActiveRecord::Schema.define(version: 20140825181631) do
 
   create_table "bookmarks", force: true do |t|
     t.integer  "tip_id"
@@ -35,12 +35,12 @@ ActiveRecord::Schema.define(version: 20140805135256) do
   create_table "users", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -53,8 +53,9 @@ ActiveRecord::Schema.define(version: 20140805135256) do
     t.string   "unconfirmed_email"
     t.string   "password"
     t.string   "password_confirmation"
-    t.integer  "bookmarks_count",        default: 0,  null: false
-    t.integer  "tips_count",             default: 0,  null: false
+    t.integer  "bookmarks_count",        default: 0,     null: false
+    t.integer  "tips_count",             default: 0,     null: false
+    t.boolean  "admin",                  default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
