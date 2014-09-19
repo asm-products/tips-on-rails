3882d3cc427b1acd7f663236cf6a7219b55e5257
Mike Bukowski <michael.r.bukowski@gmail.com>
2014-09-19 11:31:44 -0400

    Update weekly stats for Admin Dashboard
     - Refactor sql query for weekly stats
     - Add link in dashboard view to connect to weekly stats view

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -27,19 +27,15 @@ class Admin::DashboardsController < ApplicationController
 		@users = User.order(sort_column + ' ' + sort_direction).paginate(:page => params[:page], :per_page => 20)
 	end
 
-	def weekly_stats
-	# 	date = Date.today
-	# 	@selected_start_date = (date.at_beginning_of_week).to_s
-	# 	@selected_end_date = (date.at_beginning_of_week + 6.days).to_s
-	# 	# @weekly_tips = Tip.where(:created_at => @selected_start_date..@selected_end_date).count
-	# 	@other_tips = Tip.where(:created_at => @weeks).count
-	# 	@all_tips = Tip.count
-		@week = Tip.all 
-		@tips = @week.group_by { |t| t.created_at.strftime("%U %B, %Y") }
-	 # 	@weeks = (Date.parse("2014-08-04")..Date.parse("2014-12-30")).select(&:sunday?).map(&:to_s)
-		# @weekly_tips = @weeks.each
-	# @total_tips = User.where(:tips_count => @weekly_tips).size
-	# Tip.where(created => start...end)
+	def weekly_stats	
+		sql = "Select date_format(created_at, '%U %b, %Y'), COUNT(*) from tips group by week(created_at) order by week(created_at)"
+		tips_by_week = ActiveRecord::Base.connection.execute(sql)
+		@tips = []
+		total = 0
+		tips_by_week.each do |t|
+			total += t[1]
+			@tips << {week: t[0], weekly_total: t[1], running_total: total }
+		end
 	end
 
 	private
