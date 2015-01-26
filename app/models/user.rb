cb11faabfc9b15856e704c7825ca0229ba9c0c3b
Mike Bukowski <michael.r.bukowski@gmail.com>
2015-01-25 19:43:14 -0500

    Add additional validations for User model
     - Allow Owner to update user information from console without need of password.

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,8 +16,8 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(?:\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   
-  validates :password, presence: true, length: { minimum: 6 }
-  validates :password_confirmation, presence: true
+  validates :password, presence: true, length: { minimum: 6 }, on: :create
+  validates :password_confirmation, presence: true, on: :update, allow_blank: true
 
   def name 
     [first_name, last_name].join(' ')
