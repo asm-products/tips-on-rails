b1b516d504e8c3d8861f69e2e486f5edfa974d9a
Mike Bukowski <michael.r.bukowski@gmail.com>
2015-02-14 13:14:51 -0500

    Add tests for #dashboard in Admin Controller
    - Test that #dashboard instance variables aren't empty.
    - Test that each instance variable has a count greater than 1.

--- spec/controllers/admin/dashboards_controller_spec.rb
+++ spec/controllers/admin/dashboards_controller_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe Admin::DashboardsController, :type => :controller do
+describe Admin::DashboardsController do
 
+	describe "#dashboards" do
+		it "@all_users is not empty" do
+			all_users = assigns(:all_users)
+			expect([:all_users]).to_not be_empty
+		end
+		it "@all_users is assigned the total Users count" do
+			all_users = assigns(:all_users)
+			expect([:all_users].count).to be >= 1
+		end
+		it "@all_tips is not empty" do
+			all_tips = assigns(:all_tips)
+			expect([:all_tips]).to_not be_empty
+		end
+		it "@all_tips is assigned the total Tips count" do
+			all_tips = assigns(:all_tips)
+			expect([:all_tips].count).to be >= 1
+		end
+		it "@all_bookmarks is not empty" do
+			all_bookmarks = assigns(:all_bookmarks)
+			expect([:all_bookmarks]).to_not be_empty
+		end
+	end
 end
