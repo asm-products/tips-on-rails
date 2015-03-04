b3446ba3a52153f0ff2c7bf6ed1680d87e3449a2
Oto Hlincik <ohlincik@gmail.com>
2015-03-04 10:21:08 -0500

    Add github picture to user
    - Move avatar picture display functionality from helper to user
      decorator
    - Add the functionality to display github picture in addition to
      gravatar
    - Add user decorator spec

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,13 +1,13 @@
 class UserDecorator < Draper::Decorator
   delegate_all
 
-  # Define presentation-specific methods here. Helpers are accessed through
-  # `helpers` (aka `h`). You can override attributes, for example:
-  #
-  #   def created_at
-  #     helpers.content_tag :span, class: 'time' do
-  #       object.created_at.strftime("%a %m/%d/%y")
-  #     end
-  #   end
-
+  def display_avatar(options = { size: 80 })
+    if avatar == 'github'
+      avatar_url = "https://avatars.githubusercontent.com/u/#{uid}?s=#{options[:size]}"
+    else
+      gravatar_id = Digest::MD5.hexdigest(email.downcase)
+      avatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    end
+    h.image_tag(avatar_url, width: options[:size], alt: name, class: 'img-thumbnail')
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150218025117) do
+ActiveRecord::Schema.define(version: 20150304012934) do
 
   create_table "bookmarks", force: true do |t|
     t.integer  "tip_id"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20150218025117) do
     t.string   "provider"
     t.string   "uid"
     t.string   "slug"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
